b8b0656b0ee0b93b5b3efffffce0fe413128cff6
Onkar Singh <onkar1700@gmail.com>
2018-08-09 14:50:16 +0530

    add heroku with mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://onkar:Onkar-007@ds117422.mlab.com:17422/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
